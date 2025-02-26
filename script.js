625fe8d24d8cfdd600119b9b8bd18f945a613c29
Aleksandar Aleksandrov <a.alexandrov@yahoo.com>
2025-02-26 10:19:22 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function createPickel() {
+  var win = window.open('about:blank#ff');
+  win.document.body.style.margin = '0';
+  win.document.body.style.height = '100vh';
+  var iframe = win.document.createElement('iframe');
+  iframe.style.border = 'none';
+  iframe.style.width = '100%';
+  iframe.style.height = '100%';
+  iframe.style.margin = '0';
+  iframe.src = 'https://pickleservices.github.io/pickle';
+  win.document.body.appendChild(iframe);
+  }
+
+function createClassroom() {
+  var win = window.open("http://classroom.google.com");
+  win.document.body.style.margin = '0';
+  win.document.body.style.height = '100vh';
+  }
+  
